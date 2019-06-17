bfe454c88e90492c10eb84a1cfd51c95398be3ce
tianyuheng <958245365@qq.com>
2019-06-17 13:19:19 +0800

    git测试

---
+++ app/src/main/java/wlm/tyhkj/RxJavaActivity.java
@@ -0,0 +1,148 @@
+package wlm.tyhkj;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+
+
+public class RxJavaActivity extends AppCompatActivity {
+Context context;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rx_java);
+        context = this;
+//        base();
+//        bean();
+//        yanchi();
+        map();
+    }
+
+    private void map() {
+        Observable.just(1,2,3,4).map(new Function<Integer, Object>() {
+            @Override
+            public Object apply(Integer integer) throws Exception {
+                return "对Next事件作出响应" + integer;
+            }
+        }).subscribe(new Observer<Object>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Object o) {
+                Log.e("RXJAVA", "对Next事件作出响应" + o);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+
+
+    }
+
+    private void yanchi() {
+        Observable.intervalRange(0,4,2,1, TimeUnit.SECONDS).subscribe(new Observer<Long>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                Log.e("RXJAVA", "对Next事件作出响应" + aLong);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+
+    }
+
+    private void bean() {
+        List<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        Observable.fromIterable(list).subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.e("RXJAVA", "对Next事件作出响应" + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+
+
+
+    }
+
+    private void  base(){
+        Observable observable = Observable.just("A", "B", "C");
+        Observer<String> observer  =  new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String integer) {
+                Log.e("RXJAVA", "对Next事件作出响应" + integer);
+                Toast.makeText(context,String.valueOf(integer),Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+        observable.subscribe(observer);
+    }
+}
