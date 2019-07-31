aa36850d6e2bfeee1dc630e51eed4de0942d01e0
tianyuheng <958245365@qq.com>
2019-07-31 21:19:03 +0800

    笔记更新

---
+++ app/src/main/java/wlm/tyhkj/PickViewActivity.java
@@ -0,0 +1,19 @@
+package wlm.tyhkj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.bigkoo.pickerview.builder.OptionsPickerBuilder;
+import com.bigkoo.pickerview.listener.OnOptionsSelectChangeListener;
+import com.bigkoo.pickerview.view.OptionsPickerView;
+
+public class PickViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_view);
+
+    }
+}
