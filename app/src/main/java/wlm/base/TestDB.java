c04293bc4777845ddc4820fc1bac4588a7c9b675
田羽衡 <958245365@qq.com>
2019-08-22 17:39:43 +0800

    数据库示例更新

--- app/src/main/java/wlm/base/TestDB.java
+++ app/src/main/java/wlm/base/TestDB.java
@@ -1,13 +1,16 @@
 package wlm.base;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 public class TestDB {
     private SqlLiteTool tool;
     private SQLiteDatabase dbw;
-    String name ;
+    String name;
+
     public TestDB(Context context) {
         tool = new SqlLiteTool(context, "test", null, 1);
         dbw = tool.getWritableDatabase();
@@ -20,12 +23,35 @@ public class TestDB {
         dbw.endTransaction();
     }
 
-    public String readSomeThing() {
-        Cursor c = dbw.rawQuery("SELECT * FROM person", null);
-        while (c.moveToNext()){
-            name=c.getString(c.getColumnIndex("name"));
+    public String readSomeThing(String name) {
+//        表名字加上条件
+        Cursor c = dbw.rawQuery("SELECT * FROM person where name = ? ", new String[]{name});
+        while (c.moveToNext()) {
+            name = name + "______" + c.getString(c.getColumnIndex("name"));
+            name = name + "______" + c.getString(c.getColumnIndex("age"));
+            name = name + "______" + c.getString(c.getColumnIndex("sex"));
         }
         c.close();
+        Log.e("结果", name);
         return name;
     }
+
+
+    public void addData(String name, String age, String sex) {
+        ContentValues values = new ContentValues();
+        values.put("name", name);
+        values.put("age", age);
+        values.put("sex", sex);
+        dbw.insert("person", null, values);
+    }
+
+
+    public void upData(String condition, String conditionTxt, String sex) {
+        ContentValues value = new ContentValues();
+        value.put("sex", sex);
+        condition = condition + "=?";
+        dbw.update("person", value, condition, new String[]{conditionTxt});
+    }
+
+
 }
--- app/src/main/java/wlm/tyhkj/SqlActivity.java
+++ app/src/main/java/wlm/tyhkj/SqlActivity.java
@@ -16,7 +16,14 @@ public class SqlActivity extends AppCompatActivity {
     TextView add;
     @InjectView(R.id.read)
     TextView read;
+    @InjectView(R.id.tv_result)
+    TextView tvResult;
+    @InjectView(R.id.tv_updata)
+    TextView tvUpdata;
+    @InjectView(R.id.tv_deldata)
+    TextView tvDeldata;
     private TestDB db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,14 +32,22 @@ public class SqlActivity extends AppCompatActivity {
         db = new TestDB(this);
     }
 
-    @OnClick({R.id.add, R.id.read})
-    public void onClick(View view) {
+
+    @OnClick({R.id.tv_result, R.id.add, R.id.read,R.id.tv_updata,R.id.tv_deldata})
+    public void onViewClicked(View view) {
         switch (view.getId()) {
+            case R.id.tv_result:
+                break;
             case R.id.add:
-                db.addSomeThing();
+                db.addData("皮长山", "35", "男");
                 break;
             case R.id.read:
-                read.setText(db.readSomeThing());
+                tvResult.setText(db.readSomeThing("皮长山"));
+                break;
+            case R.id.tv_updata:
+                db.upData("name","皮长山","女");
+                break;
+            case R.id.tv_deldata:
                 break;
         }
     }
